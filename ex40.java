7c82191d6266af05f5aa590f6a91be1c25796f7b
rakheshprasaath <37951918+rakheshprasaath@users.noreply.github.com>
2018-08-06 19:08:10 +0530

    Add files via upload

--- ex40.java
+++ ex40.java
@@ -1,5 +1,5 @@
 import java.nio.charset.Charset;
-public class Ques40 {
+public class Exercise40 {
   public static void main(String[] args) {
 	System.out.println("List of available character sets: ");  
     for (String str : Charset.availableCharsets().keySet()) {
